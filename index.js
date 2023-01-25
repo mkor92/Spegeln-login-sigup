b750519a72927e80daa9a3edec053634ebaa4122
mkor92 <mkorhonen92@gmail.com>
2023-01-25 13:49:12 +0100

    fetch and render api working

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
+import { loadMovie, loadMovies } from "./src/JS/movies.js";
 
-const headerMenu = [
+let headerMenu = [
   {
     label: "Hem",
     link: "/",
@@ -22,48 +23,79 @@ const headerMenu = [
 const app = express();
 app.set("view engine", "ejs");
 
+function menuWithActive(items, path) {
+  return items.map((item) => ({
+    active: item.link == path,
+    ...item,
+  }));
+}
+
 app.get("/", (req, res) => {
-  res.status(200).render("index");
+  res.status(200).render("index", {
+    headerMenu: menuWithActive(headerMenu, "/"),
+  });
 });
 
-app.get("/movies", (req, res) => {
-  res.status(200).render("movies");
+app.get("/movies", async (req, res) => {
+  const movies = await loadMovies();
+  res.status(200).render("movies", {
+    headerMenu: headerMenu,
+    movies,
+  });
 });
 
 app.get("/salons", (req, res) => {
-  res.status(200).render("salons");
+  res.status(200).render("salons", {
+    headerMenu: headerMenu,
+  });
 });
 
 app.get("/about", (req, res) => {
-  res.status(200).render("about-us");
+  res.status(200).render("about-us", {
+    headerMenu: menuWithActive(headerMenu, "/about"),
+  });
 });
 
 app.get("/events", (req, res) => {
-  res.status(200).render("events");
+  res.status(200).render("events", {
+    headerMenu: headerMenu,
+  });
 });
 
 app.get("/movie-info", (req, res) => {
-  res.status(200).render("movie-info");
+  res.status(200).render("movie-info", {
+    headerMenu: headerMenu,
+  });
 });
 
 app.get("/restaurant", (req, res) => {
-  res.status(200).render("restaurant");
+  res.status(200).render("restaurant", {
+    headerMenu: headerMenu,
+  });
 });
 
 app.get("/salon-a", (req, res) => {
-  res.status(200).render("salonA");
+  res.status(200).render("salonA", {
+    headerMenu: headerMenu,
+  });
 });
 
 app.get("/salon-b", (req, res) => {
-  res.status(200).render("salonB");
+  res.status(200).render("salonB", {
+    headerMenu: headerMenu,
+  });
 });
 
 app.get("/UC", (req, res) => {
-  res.status(200).render("under-construction");
+  res.status(200).render("under-construction", {
+    headerMenu: headerMenu,
+  });
 });
 
 app.get("/booking", (req, res) => {
-  res.status(200).render("booking");
+  res.status(200).render("booking", {
+    headerMenu: headerMenu,
+  });
 });
 
 app.use("/static", express.static("./static"));
--- src/JS/movies.js
+++ src/JS/movies.js
@@ -1,4 +1,17 @@
-import { getMovies } from "./getMovies.js";
+const API_URL = "https://plankton-app-xhkom.ondigitalocean.app/api/movies ";
+
+export async function loadMovies() {
+  const res = await fetch(API_URL);
+  const payload = await res.json();
+  return payload.data;
+}
+export async function loadMovie(id) {
+  const res = await fetch(API_URL + id);
+  const payload = await res.json();
+  return payload.data;
+}
+
+/*import { getMovies } from "./getMovies.js";
 import { movieInfo } from "./movie-info.js";
 
 export async function renderMovies() {
@@ -95,4 +108,4 @@ export async function renderMovies() {
     });
   });
 }
-renderMovies();
+renderMovies();*/
