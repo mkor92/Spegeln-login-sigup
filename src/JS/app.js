6b71e534652b45d248570506cb8fc6de0489d4f5
mkor92 <mkorhonen92@gmail.com>
2023-01-28 16:50:04 +0100

    Start page doesnt show more than 8 movies

--- src/JS/app.js
+++ src/JS/app.js
@@ -60,20 +60,36 @@ app.get("/movies/:movieId", async (req, res) => {
 
 app.get("/", async (req, res) => {
   const movies = await loadMovies();
-
-  res.render("index", {
-    headerMenu: menuWithActive(headerMenu, "/"),
-    movies,
-    movieHeader: "På bion just nu",
-    movieImage0: movies[0].attributes.image.url,
-    movieTitle0: movies[0].attributes.title,
-    movieImage1: movies[1].attributes.image.url,
-    movieTitle1: movies[1].attributes.title,
-    movieImage2: movies[2].attributes.image.url,
-    movieTitle2: movies[2].attributes.title,
-    movieImage3: movies[3].attributes.image.url,
-    movieTitle3: movies[3].attributes.title,
-  });
+  let moviesArray = movies.slice(0, 8);
+  if (movies.length > 8) {
+    res.render("index", {
+      headerMenu: menuWithActive(headerMenu, "/"),
+      movies: moviesArray,
+      movieHeader: "På bion just nu",
+      movieImage0: movies[0].attributes.image.url,
+      movieTitle0: movies[0].attributes.title,
+      movieImage1: movies[1].attributes.image.url,
+      movieTitle1: movies[1].attributes.title,
+      movieImage2: movies[2].attributes.image.url,
+      movieTitle2: movies[2].attributes.title,
+      movieImage3: movies[3].attributes.image.url,
+      movieTitle3: movies[3].attributes.title,
+    });
+  } else {
+    res.render("index", {
+      headerMenu: menuWithActive(headerMenu, "/"),
+      movies,
+      movieHeader: "På bion just nu",
+      movieImage0: movies[0].attributes.image.url,
+      movieTitle0: movies[0].attributes.title,
+      movieImage1: movies[1].attributes.image.url,
+      movieTitle1: movies[1].attributes.title,
+      movieImage2: movies[2].attributes.image.url,
+      movieTitle2: movies[2].attributes.title,
+      movieImage3: movies[3].attributes.image.url,
+      movieTitle3: movies[3].attributes.title,
+    });
+  }
 });
 
 app.get("/salons", (req, res) => {
