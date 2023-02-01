48a781c1b9dfd6356bf31d5da0c37e0bcc4327e3
Fabrithegreat <fabrixio.rodrigo@gmail.com>
2023-02-01 18:17:10 +0100

    screenings routes, rendering, styling

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -6,9 +6,16 @@ const router = Router();
 router.get('/', (req, res) => {
     res.json({
         api: "welcome to the api"
-    })
+    });
 });
 
 router.get('/movies', apiCtrl.movies);
+router.get('/movies/:id/screenings', apiCtrl.movieScreenings);
+router.get('/movies/screenings', apiCtrl.screenings);
+router.get('*', (req, res)=> {
+    res.json({
+        message: "not-found"
+    }).status(404);
+})
 
 export default router;
