1b6ebcac55d330ab2aa00302ce7d7bc3343066fc
mkor92 <mkorhonen92@gmail.com>
2023-02-01 20:16:32 +0100

    routes and server logic for api setup

--- src/controllers/api.controller.js
+++ src/controllers/api.controller.js
@@ -1,8 +1,24 @@
 import { loadMovies } from "../helpers/movies.js";
+import {
+  getAllScreenings,
+  movieScreenings,
+  screeningsStartpage,
+} from "../helpers/screenings.js";
 const apiCtrl = {};
 
 apiCtrl.movies = async (req, res) => {
-    res.json(await loadMovies());
-}
+  res.json(await loadMovies());
+};
 
+apiCtrl.movieScreenings = async (req, res) => {
+  res.json(await movieScreenings(req.params.id));
+};
+
+apiCtrl.screenings = async (req, res) => {
+  res.json(await getAllScreenings());
+};
+
+apiCtrl.screeningsStartpage = async (req, res) => {
+  res.json(await screeningsStartpage());
+};
 export default apiCtrl;
--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -1,14 +1,17 @@
-import { Router } from 'express';
-import apiCtrl from './../controllers/api.controller.js';
+import { Router } from "express";
+import apiCtrl from "./../controllers/api.controller.js";
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.json({
-        api: "welcome to the api"
+router.get("/movies", apiCtrl.movies);
+router.get("/movies/:id/screenings", apiCtrl.movieScreenings);
+router.get("/screenings", apiCtrl.screenings);
+router.get("/screenings/startpage", apiCtrl.screeningsStartpage);
+router.get("*", (req, res) => {
+  res
+    .json({
+      message: "not-found",
     })
+    .status(404);
 });
-
-router.get('/movies', apiCtrl.movies);
-
 export default router;
