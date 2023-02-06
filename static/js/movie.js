14f07a211c44991c961ece9b930834505afb1fc8
jakobpalmborg <jakob.palmborg@gmail.com>
2023-02-06 11:07:33 +0100

    minor edit

--- static/js/movie.js
+++ static/js/movie.js
@@ -1,20 +1,20 @@
 const reviewContainer = document.querySelector(".review-container");
-const screeningUI = document.querySelector('.screenings');
+const screeningUI = document.querySelector(".screenings");
 const path = window.location.pathname;
 
 (async () => {
-    const id = path.split('/').pop();
-    const res = await fetch(`/api/movies/${id}/screenings`);
-    const data = await res.json();
+  const id = path.split("/").pop();
+  const res = await fetch(`/api/movies/${id}/screenings`);
+  const data = await res.json();
 
-    let template = '<h2>Filmvisningar</h2>';
-    data.map(screening => {
-        const room = screening.attributes.room;
-        let date = new Date(screening.attributes.start_time).toLocaleString();
-        date = date.substring(0, date.length - 3);
-        date = date.split(' ');
+  let template = "<h2>Filmvisningar</h2>";
+  data.map((screening) => {
+    const room = screening.attributes.room;
+    let date = new Date(screening.attributes.start_time).toLocaleString();
+    date = date.substring(0, date.length - 3);
+    date = date.split(" ");
 
-        template += `
+    template += `
         <li>
             <div>
                 <h3>${room}</h3>
@@ -23,29 +23,31 @@ const path = window.location.pathname;
             <button>Boka</button>
         </li>
         `;
-    });
-    screeningUI.insertAdjacentHTML('beforeend', template)
+  });
+  screeningUI.insertAdjacentHTML("beforeend", template);
 })();
 
 let page = 1;
 
 async function getReviews() {
-const id = path.split("/").pop();
-const res = await fetch(`/api/reviews/${id}&pagination[page]=${page}&pagination[pageSize]=5`);
-const data = await res.json();
-return data;
+  const id = path.split("/").pop();
+  const res = await fetch(
+    `/api/reviews/${id}&pagination[page]=${page}&pagination[pageSize]=5`
+  );
+  const data = await res.json();
+  return data;
 }
 
 const data2 = getReviews();
 
 data2.then((data) => {
-let template = "<h2>Recensioner</h2>";
-data.map((review) => {
-let rating = review.attributes.rating;
-let comment = review.attributes.comment;
-let author = review.attributes.author;
+  let template = "<h2>Recensioner</h2>";
+  data.map((review) => {
+    let rating = review.attributes.rating;
+    let comment = review.attributes.comment;
+    let author = review.attributes.author;
 
-template += `
+    template += `
         <li>
             <div>
                 <h3>Betyg ${rating}</h3>
@@ -54,35 +56,37 @@ template += `
             </div> 
         </li>
         `;
-});
-reviewContainer.insertAdjacentHTML("beforeend", template);
-reviewContainer.insertAdjacentHTML("beforeend", `<button class="previous-btn">previous</button>
-<button class="next-btn">next</button> `);
+  });
+  reviewContainer.insertAdjacentHTML("beforeend", template);
+  reviewContainer.insertAdjacentHTML(
+    "beforeend",
+    `<button class="next-btn">next</button> `
+  );
 
-const nextBtn = document.querySelector(".next-btn");
-nextBtn.addEventListener("click", nextReviewPage);
-const previousBtn = document.querySelector(".previous-btn");
-previousBtn.addEventListener("click", previousReviewPage);
+  const nextBtn = document.querySelector(".next-btn");
+  nextBtn.addEventListener("click", nextReviewPage);
+ 
 });
 
 async function nextReviewPage() {
-const id = path.split('/').pop();
-page++
-const res = await fetch(`/api/reviews/${id}&pagination[page]=${page}&pagination[pageSize]=5`);
-const data = await res.json();
-
-renderNextPage(data);
+  const id = path.split("/").pop();
+  page++; 
+  const res = await fetch(
+    `/api/reviews/${id}&pagination[page]=${page}&pagination[pageSize]=5`
+  );
+  const data = await res.json(); 
+  renderNextPage(data);
 }
 
 function renderNextPage(data) {
-reviewContainer.innerHTML = "";
-let template = "<h2>Recensioner</h2>";
-data.map((review) => {
-let rating = review.attributes.rating;
-let comment = review.attributes.comment;
-let author = review.attributes.author;
+  reviewContainer.innerHTML = "";
+  let template = "<h2>Recensioner</h2>";
+  data.map((review) => {
+    let rating = review.attributes.rating;
+    let comment = review.attributes.comment;
+    let author = review.attributes.author;
 
-template += `
+    template += `
         <li>
             <div>
                 <h3>Betyg ${rating}</h3>
@@ -91,47 +95,40 @@ template += `
             </div> 
         </li>
         `;
-});
-reviewContainer.insertAdjacentHTML("beforeend", template);
-reviewContainer.insertAdjacentHTML("beforeend", `<button class="previous-btn">previous</button>
-<button class="next-btn">next</button> `);
+  });
+  reviewContainer.insertAdjacentHTML("beforeend", template);
+  reviewContainer.insertAdjacentHTML(
+    "beforeend",
+    `<button class="previous-btn">previous</button>
+<button class="next-btn">next</button> `
+  );
 
-const nextBtn = document.querySelector(".next-btn");
-nextBtn.addEventListener("click", nextReviewPage);
-const previousBtn = document.querySelector(".previous-btn");
-previousBtn.addEventListener("click", previousReviewPage);
-};
-
-
-
- 
-
-
-
-
-
-
-
-
+  const nextBtn = document.querySelector(".next-btn");
+  nextBtn.addEventListener("click", nextReviewPage);
+  const previousBtn = document.querySelector(".previous-btn");
+  previousBtn.addEventListener("click", previousReviewPage);
+}
 
 async function previousReviewPage() {
-    const id = path.split('/').pop();
-    page--
-    const res = await fetch(`/api/reviews/${id}&pagination[page]=${page}&pagination[pageSize]=5`);
-    const data = await res.json();
-    
-    renderNextPage(data);
+  const id = path.split("/").pop();
+  page--;
+  const res = await fetch(
+    `/api/reviews/${id}&pagination[page]=${page}&pagination[pageSize]=5`
+  );
+  const data = await res.json();
+
+  renderNextPage(data);
 }
 
 function renderNextPage(data) {
-reviewContainer.innerHTML = "";
-let template = "<h2>Recensioner</h2>";
-data.map((review) => {
-let rating = review.attributes.rating;
-let comment = review.attributes.comment;
-let author = review.attributes.author;
+  reviewContainer.innerHTML = "";
+  let template = "<h2>Recensioner</h2>";
+  data.map((review) => {
+    let rating = review.attributes.rating;
+    let comment = review.attributes.comment;
+    let author = review.attributes.author;
 
-template += `
+    template += `
         <li>
             <div>
                 <h3>Betyg ${rating}</h3>
@@ -140,13 +137,16 @@ template += `
             </div> 
         </li>
         `;
-});
-reviewContainer.insertAdjacentHTML("beforeend", template);
-reviewContainer.insertAdjacentHTML("beforeend", `<button class="previous-btn">previous</button>
-<button class="next-btn">next</button> `);
+  });
+  reviewContainer.insertAdjacentHTML("beforeend", template);
+  reviewContainer.insertAdjacentHTML(
+    "beforeend",
+    `<button class="previous-btn">previous</button>
+<button class="next-btn">next</button> `
+  );
 
-const nextBtn = document.querySelector(".next-btn");
-nextBtn.addEventListener("click", nextReviewPage);
-const previousBtn = document.querySelector(".previous-btn");
-previousBtn.addEventListener("click", previousReviewPage);
-};
+  const nextBtn = document.querySelector(".next-btn");
+  nextBtn.addEventListener("click", nextReviewPage);
+  const previousBtn = document.querySelector(".previous-btn");
+  previousBtn.addEventListener("click", previousReviewPage);
+}
