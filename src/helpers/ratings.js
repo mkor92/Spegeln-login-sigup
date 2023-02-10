87e1989a5ebbd0b5b43ea755b044871b94f5dbc7
OlliverLar <olliver.larsson@hotmail.com>
2023-02-10 15:17:08 +0100

    Help

--- src/helpers/ratings.js
+++ src/helpers/ratings.js
@@ -4,9 +4,8 @@ const API_REVIEWS = "https://plankton-app-xhkom.ondigitalocean.app/api/reviews";
 export async function loadMovieRatings(id) {
   const res = await fetch(`${API_REVIEWS}?filters[movie]=${id}`);
   const payload = await res.json();
-  const result = payload.data.map((item) => ({
-    rating: item.attributes.rating,
-  }));
+  const result = payload.data.map((item) => (
+    item.attributes.rating));
     if (result.length >= 5) {
       let sum = result.reduce(function (a, b) {
         return a + b
@@ -16,9 +15,11 @@ export async function loadMovieRatings(id) {
       let finalResult = Math.round(tempResult * 10) / 10;
 
       console.log(finalResult);
+
+      res.status(200).send({ rating: finalResult });
     }
     else {
-      //Funktion för hämtning av IMDB-betyg ska in här.
+      
       console.log("IMDB-betyg");
     }
     return result;
