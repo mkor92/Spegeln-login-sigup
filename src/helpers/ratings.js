266e2fa46b2f8d9d4457cf8d2ba8744227ef63af
OlliverLar <olliver.larsson@hotmail.com>
2023-02-10 16:30:53 +0100

    Imdb is not done

--- src/helpers/ratings.js
+++ src/helpers/ratings.js
@@ -1,6 +1,11 @@
 import fetch from "node-fetch";
 const API_REVIEWS = "https://plankton-app-xhkom.ondigitalocean.app/api/reviews";
 
+
+                                        //i=imdbID //Alltid samma nyckel
+const IMDB_URL= "https://www.omdbapi.com/?i=      apikey=81d6c4d0"
+
+
 export async function loadMovieRatings(id) {
   const res = await fetch(`${API_REVIEWS}?filters[movie]=${id}`);
   const payload = await res.json();
@@ -15,10 +20,9 @@ export async function loadMovieRatings(id) {
       let finalResult = Math.round(tempResult * 10) / 10;
 
       console.log(finalResult);
-
-      res.status(200).send({ rating: finalResult });
     }
     else {
+        //IMDB-betyg
       
       console.log("IMDB-betyg");
     }
