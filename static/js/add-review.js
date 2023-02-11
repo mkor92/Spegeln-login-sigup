8bcfc227de95492c863ac74b8b1170f7a7914f64
Marta Anna Todorovic <martaannatodorovic@gmail.com>
2023-02-11 19:19:14 +0100

    changed body in add-review

--- static/js/add-review.js
+++ static/js/add-review.js
@@ -29,40 +29,30 @@ let sendBtn = document.querySelector("#addBtn");
 
 sendBtn.addEventListener("click", async (ev) => {
     ev.preventDefault();
-    rate = parseInt(rate.value);
-  comment = comment.value;
-  authorName = authorName.value;
 
-  const res = await fetch(`${link}/api/reviews`, {
+  const body = {
+    "data": {
+      comment: comment.value,
+      rating: parseInt(rate.value),
+      author: authorName.value,
+    }
+  }
+
+  await fetch(`${link}/api/reviews`, {
     method: "POST",
     mode: "cors",
     credentials: "same-origin",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({
-      movieID: movieId,
-      comment: comment,
-      rating: rate,
-      author: authorName,
-    }),
-  })
+    body: JSON.stringify(body) ,
+  });
 
-  .then((res) => {
-    
-    return res.json();
-  })
-
-
-
-  
 
   document.querySelector("#rate").selectedIndex = 0;
   document.querySelector("#addComment").value = "";
   document.querySelector("#addName").value = "";
 
   loadComment();
-  
-
 
 });
